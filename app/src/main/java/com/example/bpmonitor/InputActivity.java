0fbe0a79553aa623caec536bff1fcfce5093b2f9
mahin7773 <mahinmtk65@gmail.com>
2022-06-28 17:09:15 +0600

    some changes

--- app/src/main/java/com/example/bpmonitor/InputActivity.java
+++ app/src/main/java/com/example/bpmonitor/InputActivity.java
@@ -1,9 +1,14 @@
 package com.example.bpmonitor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -12,14 +17,33 @@ public class InputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
 
-        // add back arrow to toolbar
-        if (getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // Customize the back button
+        //actionBar.setHomeAsUpIndicator(R.drawable.mybutton);
+
+        // providing title for the ActionBar
+        actionBar.setTitle("New Measurements");
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button buttonCancel=findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: this.finish();
+                return true;
         }
-
+        return super.onOptionsItemSelected(item);
     }
 }
